e830355c8971a0c231d0a5243d0bed7329fb683f
roseokpe1~ <roseokpe@gmail.com>
2023-02-13 05:26:51 -0800

    delete app folder

--- app/controllers/messages_controller.rb
+++
@@ -1,9 +0,0 @@
-class MessagesController < ApplicationController
-  # GET /messages
-  def index
-    @messages = Message.all.shuffle
-
-    render json: @messages
-  end
-
-end
--- app/models/message.rb
+++
@@ -1,3 +0,0 @@
-class Message < ApplicationRecord
-    validates :message, presence: true
-end
